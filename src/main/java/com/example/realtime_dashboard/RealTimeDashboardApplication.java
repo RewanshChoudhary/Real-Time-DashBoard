dbe6ff3fecdd5a921e6e0964322ea5b0d9d09ad0
RewanshChoudhary <rewanshchoudhary57@gmail.com>
2025-07-31 01:46:23 +0530

    Starting phase

---
+++ src/main/java/com/example/realtime_dashboard/RealTimeDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtime_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realtime_dashboard/RealTimeDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtime_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
